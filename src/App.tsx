cebeb377e420df7b4b69b50a2e5646697b796b48
kowal <kowalski.kuba2005@wp.pl>
2023-03-02 15:50:29 +0100

    commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './screens/Home/Home';
+import Header from './components/Header/Header';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Header />
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen
+          name={'Home'}
+          component={Home}
+          options={{title: 'Home'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/temporary-data.ts
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    value: 'man',
+    label: 'Man',
+  },
+  {
+    value: 'woman',
+    label: 'Woman',
+  },
+  {
+    value: 'kids',
+    label: 'Kids',
+  },
+  {
+    value: 'promotion',
+    label: 'Promotions',
+  },
+];
